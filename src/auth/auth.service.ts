191e49468961b8d75d520a2a1e048b74d992b2e8
ShuVeriDa <bashtarov@outlook.com>
2023-03-08 09:48:15 +0300

    added the ability to register

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,57 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { AuthDto } from './auth.dto';
+import { PrismaService } from '../prisma.service';
+import { faker } from '@faker-js/faker';
+import { hash } from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService, private jwt: JwtService) {}
+
+  async register(dto: AuthDto) {
+    const oldUser = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+
+    if (oldUser) throw new BadRequestException('User already exists');
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        name: faker.name.firstName(),
+        avatarPath: faker.image.avatar(),
+        phone: faker.phone.number('+7 (###) ###-##-##'),
+        password: await hash(dto.password),
+      },
+    });
+
+    const tokens = await this.issueTokens(user.id);
+
+    return {
+      user: this.returnUserFields(user),
+      ...tokens,
+    };
+  }
+
+  private async issueTokens(userId: number) {
+    const data = { id: userId };
+
+    const accessToken = this.jwt.sign(data, {
+      expiresIn: '1h',
+    });
+    const refreshToken = this.jwt.sign(data, {
+      expiresIn: '7h',
+    });
+
+    return { accessToken, refreshToken };
+  }
+
+  private returnUserFields(user: User) {
+    return {
+      id: user.id,
+      email: user.email,
+    };
+  }
+}
